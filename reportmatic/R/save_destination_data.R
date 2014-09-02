dde79cfd86549fc26a083c85808f4196c5fa7fe3
Brian Smith <brian.smith@sojern.com>
2014-09-02 15:27:43 -0700

    namespace change

--- reportmatic/R/save_destination_data.R
+++ reportmatic/R/save_destination_data.R
@@ -6,7 +6,7 @@
 #' @export
 #' @examples
 #' save_destination_data()
-load_exposed_uu_counts <- function(file){
+save_destination_data <- function(file){
 	uu_count <- as.data.frame(read.csv(file, header = TRUE, sep = ",", quote = "\""))
 	exposed_uu_count <- as.data.frame(as.numeric(as.character(uu_count[1,2])))
 }
