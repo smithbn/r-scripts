6bd4a1cc84840bd247413dde854f51e9c20bb006
Brian Smith <brian.smith@sojern.com>
2014-09-02 13:22:58 -0700

    Changed to RMySQL from SQLITE

--- reportmatic/R/save_destination_data.R
+++ reportmatic/R/save_destination_data.R
@@ -10,11 +10,9 @@ save_destination_data <- function(file, ...){
   if(!grepl(".csv$", file)){
     stop("Uploaded file must be a .csv file!")
   }
-    require('sqldf')
-	
-	sqldf("attach '/home/bsmith/databases/destinationdb.sqlite' as new")
-
-	sqldf("create table event_data(experiment_bucket TEXT, profileid, event_date TEXT, event_type TEXT, partner TEXT, origin_airport TEXT, destination_airport TEXT, departure_date TEXT, return_date TEXT, number_of_travelers INTEGER, hotel_city TEXT, hotel_state TEXT, hotel_country TEXT, check_in_date TEXT, check_out_date TEXT, number_of_rooms INTEGER, rental_city TEXT, rental_dropoff_city TEXT, rental_pickup_date TEXT, rental_dropoff_date TEXT, vacation_airport_origin TEXT, vacation_airport_destination TEXT, vacation_departure_date TEXT, vacation_return_date TEXT)", dbname = "/home/bsmith/databases/destinationdb.sqlite")
-
-	read.csv.sql(file, header = TRUE, sep = ",", filter = list('gawk -f prog', prog = '{ gsub(/"/, ""); print }'), row.names=FALSE, comment.char = "", eol="\n", sql = "replace into event_data select * from file", dbname = "/home/bsmith/databases/destinationdb.sqlite")
+    require('RMySQL')
+	mydb = dbConnect(MySQL(), user='root', password='O87RlR0lbe', dbname='destinationdb', host='localhost')
+	rs = dbSendQuery(mydb, "select * from event_data")
+	data = fetch(rs, n=-1)
+	invisible()
 }
