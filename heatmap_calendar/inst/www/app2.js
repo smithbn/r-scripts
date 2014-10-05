0956b9870c1349fdf20f2f8b09e72a06bd47a0eb
Brian Smith <brian.smith@sojern.com>
2014-10-05 13:18:40 -0700

    Updated js

--- heatmap_calendar/inst/www/app2.js
+++ heatmap_calendar/inst/www/app2.js
@@ -32,7 +32,10 @@ $(document).ready(function(){
 
     //read the session properties (just for fun)
     $("#printlink").attr("href", session.getLoc() + "/zip");
-	$("#plotdiv").attr("href", session.getFile("heatmap.png"));
+	$("#plotdiv").attr("href", session.getFile("heatmap.png", function(chart){
+        successmsg(chart);
+      });
+	  );
 })
   }
 
