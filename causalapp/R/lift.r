5baecc5a72caac3f256a9bfa7096ff1718ddac3f
Brian Smith <brian.smith@sojern.com>
2014-11-06 13:02:13 -0800

    Updated lift.r

--- causalapp/R/lift.r
+++ causalapp/R/lift.r
@@ -6,16 +6,25 @@
 #' @export
 #' @examples
 #' lift()
-lift <- function(causal_data, pre_period_start_date, pre_period_end_date, post_period_start_date, post_period_end_date, season_number, season_duration){
+lift <- function(causal_data, data_start_date, test_start_date, season_number, season_duration){
   require('CausalImpact')
+  require('lubridate')
   causal_data <- causal_data
-  pre.period <- c(pre_period_start_date,pre_period_end_date)
+  pre_period_end_date <- (ymd(test_start_date) - ymd(data_start_date)) - 1
+  post_period_start_date <- pre_period_end_date + 1
+  post_period_end_date <- length(causal_data[,1])
+  pre.period <- c(1,pre_period_end_date)
   post.period <- c(post_period_start_date,post_period_end_date)
   impact <- CausalImpact(causal_data, pre.period, post.period, model.args = list(nseasons = season_number, season.duration = season_duration))
   options(max.print=99999999);
   options(width=120);
   print(summary(impact))
   print(summary(impact, "report"))
+  png("dataplot.png", width= 1500)
+      print(
+        matplot(causal_data, type = "l")
+    )
+    dev.off()
   png("impactplot.png", width= 1500)
     print(
         plot(impact)
